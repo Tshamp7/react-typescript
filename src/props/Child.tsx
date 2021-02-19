dd552d4ac8a42c0419815289d841f4b6fdb99970
Thomas Shamp <T.shamp7@gmail.com>
2021-02-19 09:41:59 -0800

    add various examples of defining react components with TS

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,7 +1,14 @@
 interface ChildProps {
   color: string;
 }
-
+// While the below example works, it does not let TS know that this is a React Component.
 export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>;
+};
+
+// This syntax lets TS know that this is in fact a React Component.
+// The method which you choose to use depends on if you need to call any methods that are unique to React components
+// while using typescript.
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
   return <div>{color}</div>;
 };
